703e7576289aba383128420b1c639d1461f9e4c5
zjl <zjl@qq.com>
2019-09-24 11:39:11 +0800

    添加

--- D1/src/Demo01.java
+++
@@ -1,7 +0,0 @@
-public class Demo01 {
-	public static void main(String[] args){
-		System.out.println("HelloWord");
-		System.out.println(100);
-		System.out.println('a');
-	}
-}
--- D1/src/Demo02.java
+++
@@ -1,8 +0,0 @@
-public class Demo02{
-public static void main(String[]args){
-	System.out.println("HelloWorld");
-	System.out.println("H");
-	System.out.println("5+5="+5+5);
-}	
-
-}
